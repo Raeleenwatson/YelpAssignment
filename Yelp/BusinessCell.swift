2e0be1861c7b329ca04e648545ac28c80d201d2f
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-09-19 09:55:38 -0700

    At 11:53 of video

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,39 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Raeleen Watson on 9/19/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
