8d6c6fec033137cfa61071410245a12075696f7b
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-09-20 23:45:59 -0700

    At part 2 12:53, check constraints

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -30,14 +30,28 @@ class BusinessCell: UITableViewCell {
             distanceLabel.text = business.distance
         }
     }
+
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         thumbImageView.layer.cornerRadius = 3
         thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        
+        
     }
 
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        
+    }
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
